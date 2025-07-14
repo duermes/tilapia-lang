3318c3b9a33193e6c3aa7a9d663b38d5a35b7797
duermes <iloveposhitos@gmail.com>
2025-07-14 15:01:15 -0500

    asd

--- lib/codegenx86.ml
+++ lib/codegenx86.ml
@@ -5,77 +5,81 @@ open Buffer
 let code = create 50
 let data_declr = create 50
 
-
 let op_instructions = function
   | Plus -> "\tadd rax, rbx\n"
   | Minus -> "\tsub rax, rbx\n"
   | Times -> "\timul rax, rbx \n"
-  | Divide ->  "\txor rdx, rdx\n" ^ "\tidiv rbx\n"
-  | Mod     -> "\txor rdx, rdx\n" ^ "\txidiv rbx\n" ^ "\txmov rax, rdx\n"  (* rdx has module *)
-  | Eq      -> "\tcmp rax, rbx\n" ^ "\txsete al\n" ^ "\txmovzx rax, al\n"           (* rax = (rax == rbx) ? 1 : 0 *)
-  | GT      -> "\tcmp rax, rbx\n" ^ "\txsetg al\n" ^ "\txmovzx rax, al\n"           (* rax = (rax > rbx) ? 1 : 0 *)
-  | LT      -> "\tcmp rax, rbx\n" ^ "\txsetl al\n" ^ "\txmovzx rax, al\n"           (* rax = (rax < rbx) ? 1 : 0 *)
-  | NEq     -> "\tcmp rax, rbx\n" ^ "\txsetne al\n" ^ "\txmovzx rax, al\n"          (* rax = (rax != rbx) ? 1 : 0 *)
-  | GEq     -> "\tcmp rax, rbx\n" ^ "\txsetge al\n" ^ "\txmovzx rax, al\n"          (* rax = (rax >= rbx) ? 1 : 0 *)
-  | LEq     -> "\tcmp rax, rbx\n" ^ "\txsetle al\n" ^ "\txmovzx rax, al\n"          (* rax = (rax <= rbx) ? 1 : 0 *)
-  | LAnd     -> "\tand rax, rbx\n"
-  | LOr      -> "\tor rax, rbx\n"
+  | Divide -> "\txor rdx, rdx\n" ^ "\tidiv rbx\n"
+  | Mod ->
+      "\txor rdx, rdx\n" ^ "\txidiv rbx\n"
+      ^ "\txmov rax, rdx\n" (* rdx has module *)
+  | Eq ->
+      "\tcmp rax, rbx\n" ^ "\txsete al\n"
+      ^ "\txmovzx rax, al\n" (* rax = (rax == rbx) ? 1 : 0 *)
+  | GT ->
+      "\tcmp rax, rbx\n" ^ "\txsetg al\n"
+      ^ "\txmovzx rax, al\n" (* rax = (rax > rbx) ? 1 : 0 *)
+  | LT ->
+      "\tcmp rax, rbx\n" ^ "\txsetl al\n"
+      ^ "\txmovzx rax, al\n" (* rax = (rax < rbx) ? 1 : 0 *)
+  | NEq ->
+      "\tcmp rax, rbx\n" ^ "\txsetne al\n"
+      ^ "\txmovzx rax, al\n" (* rax = (rax != rbx) ? 1 : 0 *)
+  | GEq ->
+      "\tcmp rax, rbx\n" ^ "\txsetge al\n"
+      ^ "\txmovzx rax, al\n" (* rax = (rax >= rbx) ? 1 : 0 *)
+  | LEq ->
+      "\tcmp rax, rbx\n" ^ "\txsetle al\n"
+      ^ "\txmovzx rax, al\n" (* rax = (rax <= rbx) ? 1 : 0 *)
+  | LAnd -> "\tand rax, rbx\n"
+  | LOr -> "\tor rax, rbx\n"
 
-let codegenx86_print _ =
-  "\tmov rax, 1\n \tmov rdi, 1"
-  |> add_string code
+let codegenx86_print _ = "\tmov rax, 1\n \tmov rdi, 1" |> add_string code
 
 let print_string n =
-  "\tmov rsi, "^ n ^ "\n\tmov rdx, " ^ n ^ "_len\n\tsyscall\n"
+  "\tmov rsi, " ^ n ^ "\n\tmov rdx, " ^ n ^ "_len\n\tsyscall\n"
   |> add_string code
 
 (*Numbers are printed during assembly*)
-let print_word n = 
-  "\t print \" \", "^ n ^ "\n"
-  |> add_string code
+let print_word n = "\t print \" \", " ^ n ^ "\n" |> add_string code
 
 (*constant build*)
-let asm_word name n =
-  "\t" ^ name ^ ": dq " ^ n ^ "\n"
-  |> add_string data_declr
+let asm_word name n = "\t" ^ name ^ ": dq " ^ n ^ "\n" |> add_string data_declr
 
 let asm_bool name n =
   let v = if n then "1" else "0" in
-  "\t"^ name ^ ": dq " ^ v ^ "\n"
-  |> add_string data_declr
+  "\t" ^ name ^ ": dq " ^ v ^ "\n" |> add_string data_declr
 
 let asm_bytestring name n =
-  "\t" ^ name ^ ": db " ^ n ^ ", 10\n" 
-  ^ "\t"^ name ^"_len = $ -" ^ n
+  "\t" ^ name ^ ": db " ^ n ^ ", 10\n" ^ "\t" ^ name ^ "_len = $ -" ^ n
   |> add_string data_declr
 
 (*Push to rax var to use*)
 let asm_push_var name =
-"\tmov rax, [" ^ name ^ "]\n\tpush rax\n"
-|> add_string code
+  "\tmov rax, [" ^ name ^ "]\n\tpush rax\n" |> add_string code
+
 let asm_op op =
   "    pop rbx\n" ^ "    pop rax\n" ^ op_instructions op ^ "    push rax\n"
   |> add_string code
 
-
 let asm_binop op =
   "    pop rbx\n" ^ "    pop rax\n" ^ op_instructions op ^ "    push rax\n"
   |> add_string code
-
-
 
 let rec codegenx86_stmt stmt =
   match stmt with
   | Ignore (Literal (Word n)) ->
       add_string code ("\tmov rax, " ^ Int64.to_string n ^ "\n")
   | _ -> ()
+
 let codegenx86_expr expr =
-match expr with
-| Literal (Word n) ->
-    add_string code ("\tmov rax, " ^ Int64.to_string n ^ "\n")
-| _ -> () 
+  match expr with
+  | Literal (Word n) ->
+      add_string code ("\tmov rax, " ^ Int64.to_string n ^ "\n")
+  | _ -> ()
 
 let codegenx86_args args = ()
+
 let codegenx86_func (FunDef { id; args; body; retval; _ }) =
   add_string code ("; funcion: " ^ id ^ "\n");
   codegenx86_args args;
@@ -83,18 +87,15 @@ let codegenx86_func (FunDef { id; args; body; retval; _ }) =
   (match retval with Some e -> codegenx86_expr e | None -> ());
   add_string code ("; fin funcion: " ^ id ^ "\n")
 
-
 (*toplevel*)
 let codegenx86_prog prog =
   let found_main = ref false in
-  Array.iter (function
-    | FunDef f when f.id = "main" ->
-        found_main := true
-    | FunDef f->
-        codegenx86_func (FunDef f)
-  ) prog;
-  if not !found_main then
-    failwith "Codegenx86 requires a 'main' function."
+  Array.iter
+    (function
+      | FunDef f when f.id = "main" -> found_main := true
+      | FunDef f -> codegenx86_func (FunDef f))
+    prog;
+  if not !found_main then failwith "Codegenx86 requires a 'main' function."
 
 let compile prog =
   reset code;
--- lib/template.ml
+++ lib/template.ml
@@ -1,24 +1,25 @@
-let codegen_prefix = "\n
-format ELF64 executable 3
-\n 
-macro print description,number\n
-{\n
-   display description\n
-   value=number\n
-   pos=1000\n
-   repeat 4\n
-      \tdigit=value/pos\n
-      \tvalue=value-(digit*pos)\n
-      \tpos=pos/10\n
-      \tdisplay ('0'+digit)\n
-   end repeat\n
-   display $d,$a\n
-}\n 
-\nsegment readable writeable
-\nbuffer rb 20
-\nnewline db 10\n
+let codegen_prefix =
+  "\n\n\
+   format ELF64 executable 3\n\n\
+  \ \n\
+   macro print description,number\n\n\
+   {\n\n\
+  \   display description\n\n\
+  \   value=number\n\n\
+  \   pos=1000\n\n\
+  \   repeat 4\n\n\
+  \      \tdigit=value/pos\n\n\
+  \      \tvalue=value-(digit*pos)\n\n\
+  \      \tpos=pos/10\n\n\
+  \      \tdisplay ('0'+digit)\n\n\
+  \   end repeat\n\n\
+  \   display $d,$a\n\n\
+   }\n\
+  \ \n\n\
+   segment readable writeable\n\n\
+   buffer rb 20\n\n\
+   newline db 10\n\n\n"
 
-"
 let codegen_main = "\nsegment readable executable\nentry main \nmain: \n\n"
 
 let codegen_suffix =
