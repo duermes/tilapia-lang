20aaf5a13b6145997d2e2d0fe6be31b02f1902b2
quadragon <20429861-quadragon@users.noreply.gitlab.com>
2025-07-14 16:23:18 -0500

    lexer: Revert naming change for simplicity

--- lib/driver.ml
+++ lib/driver.ml
@@ -1,6 +1,6 @@
 let parse_or_error lexbuf =
   try Parser.root Lexer.token lexbuf |> Result.ok with
-  | Lexer.LexError msg -> "Lexing error: " ^ msg |> Result.error
+  | Lexer.Error msg -> "Lexing error: " ^ msg |> Result.error
   | Parser.Error ->
       let pos = Lexing.lexeme_start_p lexbuf in
       let col = pos.pos_cnum - pos.pos_bol in
